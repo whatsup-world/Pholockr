02fc25838a8a5cc76cdf113e9f459dc6d4ba8ff6
Edward <wedward1024@gmail.com>
2022-06-25 20:49:55 -0700

    phase 0 done

---
+++ frontend/src/store/session.js
@@ -0,0 +1,11 @@
+import { csrfFetch } from './csrf';
+
+const SET_USER = 'session/setUser';
+const REMOVE_USER = 'session/removeUser';
+
+const setUser = (user) => {
+  return {
+    type: SET_USER,
+    payload: user,
+  }
+}
